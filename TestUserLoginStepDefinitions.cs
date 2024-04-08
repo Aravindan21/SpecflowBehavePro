10cdc651ae7f4048c29a884c0416d8ed5f035920
Aravindan <akaivalliam@liventus.com>
2024-04-08 16:33:24 +0530

    upload files

---
+++ TestUserLoginStepDefinitions.cs
@@ -0,0 +1,40 @@
+using FluentAssertions;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SampleProject
+{
+    [Binding]
+    public class TestUserLoginStepDefinitions
+    {
+        string _userName, _password;
+
+        [Given(@"User enters '([^']*)' and '([^']*)'")]
+        public void GivenUserEntersAnd(string username, string password)
+        {
+            _userName = username;
+            _password = password;
+        }
+
+        [When(@"User clicks on login")]
+        public void WhenUserClicksOnLogin()
+        {
+            
+        }
+
+        [Then(@"Response should be '([^']*)'")]
+        public void ThenResponseShouldBeFalse(bool success)
+        {
+            var result = AuthenticateUsers.AuthenticateUser(_userName, _password);
+            result.Should().Be(success);
+            
+        }
+
+        [Then(@"Logged in Successfully")]
+        public void ThenLoggedInSuccessfully()
+        {
+
+        }
+
+    }
+}
